a726e5fd383243c3c6a0cf77bea682951eb08836
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-09-28 12:05:56 +0800

    FIRST commit

---
+++ src/main/java/com/tomatopay/domaintransactions/DomaintransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.tomatopay.domaintransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaintransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaintransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomatopay/domaintransactions/DomaintransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomatopay.domaintransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomaintransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
